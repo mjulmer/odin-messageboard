3d1fdb8c433a0c979f6892fff860d6e68f18dba1
M Ulmer <mju9999@gmail.com>
2025-08-03 16:52:30 -0700

    Add template for home page, show dummy message data

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-import express from "express";
+const express = require("express");
+const path = require("node:path");
 const app = express();
 
-app.get("/", (req, res) => res.send("Home page"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+const messages = [
+  {
+    text: "High: finished the CSS module",
+    user: "Anora",
+    added: new Date(),
+  },
+  {
+    text: "Low: got stuck trying to install Postman",
+    user: "Thara",
+    added: new Date(),
+  },
+];
+
+app.get("/", (req, res) =>
+  res.render("index", {
+    messages: messages,
+  })
+);
 app.get("/new", (req, res) => res.send("New post form"));
 
 app.use((err, req, res, next) => {
