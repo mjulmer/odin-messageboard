82f151cd0817616ec837ba66375256b5aea72fc6
M Ulmer <mju9999@gmail.com>
2025-08-03 17:25:01 -0700

    Add form for new messages and display new data on main page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(express.urlencoded({ extended: true }));
 
 const messages = [
   {
@@ -23,10 +24,19 @@ app.get("/", (req, res) =>
     messages: messages,
   })
 );
-app.get("/new", (req, res) => res.send("New post form"));
+app.get("/new", (req, res) => res.render("form"));
+app.post("/new", (req, res) => {
+  console.log("Ready to add the new message");
+  messages.push({
+    text: req.body.message_body,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
 
 app.use((err, req, res, next) => {
-  console.error(err);
+  console.error("Error was THIS: " + err);
   res.status(500).send(err);
 });
 
